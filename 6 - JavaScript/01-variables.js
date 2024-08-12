92fc08fb994372ca78fc3350517a54a06c798f46
Jared Neil A. Hortaleza <jaredneilhortaleza11@gmail.com>
2024-08-12 16:37:37 +0800

    finished intermediate CSS, started JS

---
+++ 6 - JavaScript/01-variables.js
@@ -0,0 +1,19 @@
+console.log("Hello World!");
+
+// Variables = store values
+// let, const, *var 
+
+// let - mutable/editable value
+let firstName = "Jared";
+console.log("Firstname:", firstName);
+
+firstName = "Ariston";
+console.log("Firstname:", firstName);
+
+// const - immutable/uneditable value
+const lastName = "Hortaleza";
+console.log("Lastname:", lastName);
+
+// This gives us an error!
+// lastName = "Catipay";
+// console.log("Lastname:", lastName);
---
+++ 6 - JavaScript/02-datatypes.js
@@ -0,0 +1,26 @@
+// Data Types - Primitive
+
+// Number: Represent numeric values (35, 3.14)
+let age = -22.3;
+console.log("Age:", age);
+console.log("Typeof age:", typeof age);
+
+// Strings: Represent textual values ("abc", 'def123')
+let name = "John";
+console.log("Name:", name);
+console.log("Typeof name:", typeof name);
+
+// Boolean: Either true or false
+let isAdmin = true;
+console.log("IsAdmin:", isAdmin);
+console.log("Typeof isAdmin:", typeof isAdmin);
+
+// Null: Represent INTENTIONAL absence of value
+let car = null;
+console.log("Car?", car);
+console.log("Typeof car:", typeof car);
+
+// Undefined: Representing UNINTENTIONAL absence of value
+let city;
+console.log("City?", city);
+console.log("Typeof city:", typeof city);
